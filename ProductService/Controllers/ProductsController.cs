d82fb1e3bf39f7f1e25d9d23a4e4dc90bc462f7b
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 20:09:55 +0200

    dobavljac proizvoda

--- ProductService/Controllers/ProductsController.cs
+++ ProductService/Controllers/ProductsController.cs
@@ -28,6 +28,15 @@ namespace ProductService.Controllers
         }
 
 
+        // GET /Products(1)/Supplier
+        [EnableQuery]
+        public SingleResult<Supplier> GetSupplier([FromODataUri] int key)
+        {
+            var result = db.Products.Where(m => m.Id == key).Select(m => m.Supplier);
+            return SingleResult.Create(result);
+        }
+
+
         [EnableQuery]
         public IQueryable<Product> Get()
         {
