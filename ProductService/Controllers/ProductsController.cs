4d6fd97f424a36f1be57f071f30e9d13a32336c0
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 20:14:11 +0200

    brisanje odnosa proizvoda i dobavljaca

--- ProductService/Controllers/ProductsController.cs
+++ ProductService/Controllers/ProductsController.cs
@@ -37,6 +37,61 @@ namespace ProductService.Controllers
         }
 
 
+        [AcceptVerbs("POST", "PUT")]
+        public async Task<IHttpActionResult> CreateRef([FromODataUri] int key,
+        string navigationProperty, [FromBody] Uri link)
+        {
+            var product = await db.Products.SingleOrDefaultAsync(p => p.Id == key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            switch (navigationProperty)
+            {
+                case "Supplier":
+                    // Note: The code for GetKeyFromUri is shown later in this topic.
+                    var relatedKey = Helpers.GetKeyFromUri<int>(Request, link);
+                    var supplier = await db.Suppliers.SingleOrDefaultAsync(f => f.Id == relatedKey);
+                    if (supplier == null)
+                    {
+                        return NotFound();
+                    }
+
+                    product.Supplier = supplier;
+                    break;
+
+                default:
+                    return StatusCode(HttpStatusCode.NotImplemented);
+            }
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
+        public async Task<IHttpActionResult> DeleteRef([FromODataUri] int key,
+        string navigationProperty, [FromBody] Uri link)
+        {
+            var product = db.Products.SingleOrDefault(p => p.Id == key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            switch (navigationProperty)
+            {
+                case "Supplier":
+                    product.Supplier = null;
+                    break;
+
+                default:
+                    return StatusCode(HttpStatusCode.NotImplemented);
+            }
+            await db.SaveChangesAsync();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+
         [EnableQuery]
         public IQueryable<Product> Get()
         {
