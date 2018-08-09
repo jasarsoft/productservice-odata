784a921148e092d6f5c65d27e71cb5e1210f22c8
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 19:57:42 +0200

    kontroler dobavljaca

---
+++ ProductService/Controllers/SuppliersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.OData;
+using ProductService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductService.Controllers
+{
+    public class SuppliersController : ODataController
+    {
+        ProductsContext db = new ProductsContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
