3f81854ab63eff576c7aa5412b811f75e30a6977
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 20:05:04 +0200

    crud metode kontrolera dobavljac

--- ProductService/Controllers/SuppliersController.cs
+++ ProductService/Controllers/SuppliersController.cs
@@ -4,6 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace ProductService.Controllers
 {
@@ -11,10 +17,112 @@ namespace ProductService.Controllers
     {
         ProductsContext db = new ProductsContext();
 
+        private bool SupplierExists(int key)
+        {
+            return db.Suppliers.Any(p => p.Id == key);
+        }
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
             base.Dispose(disposing);
+        }
+
+
+        [EnableQuery]
+        public IQueryable<Supplier> Get()
+        {
+            return db.Suppliers;
+        }
+        [EnableQuery]
+        public SingleResult<Supplier> Get([FromODataUri] int key)
+        {
+            IQueryable<Supplier> result = db.Suppliers.Where(p => p.Id == key);
+            return SingleResult.Create(result);
+        }
+
+
+        public async Task<IHttpActionResult> Post(Supplier supplier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Suppliers.Add(supplier);
+            await db.SaveChangesAsync();
+            return Created(supplier);
+        }
+
+
+        public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<Supplier> supplier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var entity = await db.Suppliers.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            supplier.Patch(entity);
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(entity);
+        }
+        public async Task<IHttpActionResult> Put([FromODataUri] int key, Supplier update)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Updated(update);
+        }
+
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var supplier = await db.Suppliers.FindAsync(key);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            db.Suppliers.Remove(supplier);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
         }
     }
 }
