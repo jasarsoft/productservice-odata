126e0ed214fd14294185a5f5159fa5e74f9be54c
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 20:10:48 +0200

    kolekcija dobavljaca

--- ProductService/Controllers/SuppliersController.cs
+++ ProductService/Controllers/SuppliersController.cs
@@ -29,6 +29,15 @@ namespace ProductService.Controllers
         }
 
 
+        // GET /Suppliers(1)/Products
+        [EnableQuery]
+        public IQueryable<Product> GetProducts([FromODataUri] int key)
+        {
+            return db.Suppliers.Where(m => m.Id.Equals(key)).SelectMany(m => m.Products);
+        }
+
+
+
         [EnableQuery]
         public IQueryable<Supplier> Get()
         {
