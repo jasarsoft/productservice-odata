7321041d21249eee0e8350ee9140d8db24261be2
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 19:48:51 +0200

    enitetski skup

--- ProductService/Controllers/ProductsController.cs
+++ ProductService/Controllers/ProductsController.cs
@@ -39,5 +39,17 @@ namespace ProductService.Controllers
             IQueryable<Product> result = db.Products.Where(p => p.Id == key);
             return SingleResult.Create(result);
         }
+
+
+        public async Task<IHttpActionResult> Post(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Products.Add(product);
+            await db.SaveChangesAsync();
+            return Created(product);
+        }
     }
 }
