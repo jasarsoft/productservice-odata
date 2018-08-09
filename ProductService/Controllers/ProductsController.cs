5cacbab01cd620b58e824ddce594275d5ba3b87b
Edin Jašarević <edinjasar14@gmail.com>
2018-08-09 19:50:10 +0200

    brisanje entiteta

--- ProductService/Controllers/ProductsController.cs
+++ ProductService/Controllers/ProductsController.cs
@@ -110,5 +110,18 @@ namespace ProductService.Controllers
             }
             return Updated(update);
         }
+
+
+        public async Task<IHttpActionResult> Delete([FromODataUri] int key)
+        {
+            var product = await db.Products.FindAsync(key);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
     }
 }
